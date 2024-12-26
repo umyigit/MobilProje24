447b3cca1369c43aeeb6eb0910a5ec11c66a325b
saliha <salihaunersoy@outlook.com>
2024-12-26 17:45:42 +0300

    LocalDatabase

---
+++ pythondiyabetapp/config.py
@@ -0,0 +1,2 @@
+class Config:
+    MONGO_URI = "mongodb+srv://medineeuzunn:lkHwgiZMseGb1SSl@diyabetapp.lo7nogg.mongodb.net/diyabetapp?retryWrites=true&w=majority"
---
+++ pythondiyabetapp/run.py
@@ -0,0 +1,4 @@
+from app import app
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.36', port=5000, debug=True)
